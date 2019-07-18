fccec99ef64004e59ec72df8cc8fb305fa47225c
huihua <huihua.zhao@tri.global>
2019-07-18 16:36:28 -0700

    Add camera to the scene

--- examples/coke_cam/coke_cam_demo.cc
+++ examples/coke_cam/coke_cam_demo.cc
@@ -5,8 +5,12 @@
 
 #include <gflags/gflags.h>
 
+#include "robotlocomotion/header_t.hpp"
 #include "drake/common/drake_assert.h"
 #include "drake/common/find_resource.h"
+#include "drake/geometry/dev/render/render_engine.h"
+#include "drake/geometry/dev/render/render_engine_vtk.h"
+#include "drake/geometry/dev/scene_graph.h"
 #include "drake/geometry/geometry_visualization.h"
 #include "drake/geometry/scene_graph.h"
 #include "drake/lcm/drake_lcm.h"
@@ -14,6 +18,9 @@
 #include "drake/multibody/parsing/parser.h"
 #include "drake/systems/analysis/simulator.h"
 #include "drake/systems/framework/diagram_builder.h"
+#include "drake/systems/sensors/dev/rgbd_camera.h"
+#include "drake/systems/sensors/image_to_lcm_image_array_t.h"
+#include "drake/systems/sensors/pixel_types.h"
 
 namespace drake {
 namespace examples {
@@ -33,9 +40,12 @@ DEFINE_double(inclined_plane_coef_kinetic_friction, 0.3,
               "When time_step > 0, this value is ignored.  Only the "
               "coefficient of static friction is used in fixed-time step.");
 DEFINE_double(gravity, 9.8, "Value of gravity in the direction of -z.");
-//
+
+// Urdf path of the coke can and a reference geometry.
 static const char* const kCokeCanUrdfPath =
     "drake/examples/coke_cam/urdf/coke_can.urdf";
+static const char* const kUnitBoxUrdfPath =
+    "drake/examples/coke_cam/urdf/couch_rough.urdf";
 
 int do_main() {
   DRAKE_DEMAND(FLAGS_simulation_time > 0.0);
@@ -55,10 +65,16 @@ int do_main() {
 
   multibody::Parser parser(&plant);
 
-  const std::string full_name = FindResourceOrThrow(kCokeCanUrdfPath);
+  const std::string can_full_name = FindResourceOrThrow(kCokeCanUrdfPath);
 
-  auto coke_model_instance_index = parser.AddModelFromFile(full_name);
-  (void)coke_model_instance_index;
+  auto can_model_instance_index = parser.AddModelFromFile(can_full_name);
+  (void)can_model_instance_index;
+
+  // Add a reference unit cubit.
+  const std::string cube_full_name = FindResourceOrThrow(kUnitBoxUrdfPath);
+
+  auto cube_model_instance_index = parser.AddModelFromFile(cube_full_name);
+  (void)cube_model_instance_index;
 
   // Add half space plane and gravity.
   const drake::multibody::CoulombFriction<double> coef_friction_inclined_plane(
@@ -79,6 +95,72 @@ int do_main() {
       scene_graph.get_source_pose_port(plant.get_source_id().value()));
   geometry::ConnectDrakeVisualizer(&builder, scene_graph);
 
+  // Create scene render.
+  const std::string default_render_name = "coke_cam_render";
+  geometry::dev::SceneGraph<double>* render_scene_graph{};
+  render_scene_graph =
+      builder.template AddSystem<drake::geometry::dev::SceneGraph>();
+  render_scene_graph->set_name("dev_scene_graph_for_rendering");
+  render_scene_graph->AddRenderer(
+      default_render_name,
+      std::make_unique<drake::geometry::dev::render::RenderEngineVtk>());
+  render_scene_graph->CopyFrom(scene_graph);
+
+  builder.Connect(
+      plant.get_geometry_poses_output_port(),
+      render_scene_graph->get_source_pose_port(plant.get_source_id().value()));
+
+  // Setup a camera.
+  const std::string camera_name = "ground_cam";
+  const double kFocalY = 645;
+  const int kHeight = 480;
+  const int kWidth = 848;
+  const double fov_y = std::atan(kHeight / 2. / kFocalY) * 2;
+  drake::geometry::dev::render::DepthCameraProperties camera_properties(
+      kWidth, kHeight, fov_y, default_render_name, 0.1, 2.0);
+  // Determine the camera to world frame orientation.
+  drake::math::RotationMatrix<double> R_AB =
+      drake::math::RotationMatrix<double>::MakeFromOrthonormalRows(
+          Eigen::Vector3d{0, -1, 0}, Eigen::Vector3d{1, 0, 0},
+          Eigen::Vector3d{0, 0, 1});
+  // Rotate the camera to point downward slightly.
+  drake::math::RotationMatrix<double> R_WA(
+      Eigen::AngleAxisd(0, Eigen::Vector3d::UnitX()));
+
+  auto& cam_parent_frame = plant.world_body().body_frame();
+  const drake::optional<drake::geometry::FrameId> parent_body_id =
+      plant.GetBodyFrameIdIfExists(cam_parent_frame.body().index());
+  DRAKE_THROW_UNLESS(parent_body_id.has_value());
+  const drake::Isometry3<double> X_PC =
+      cam_parent_frame.GetFixedPoseInBodyFrame() *
+      math::RigidTransform<double>(R_WA * R_AB,
+                                   Eigen::Vector3d(0.0, -1.0, 0.1));
+
+  auto camera =
+      builder.template AddSystem<drake::systems::sensors::dev::RgbdCamera>(
+          camera_name, parent_body_id.value(), X_PC, camera_properties, false);
+  builder.Connect(render_scene_graph->get_query_output_port(),
+                  camera->query_object_input_port());
+
+  // Publish the image.
+  auto image_to_lcm_image_array =
+      builder
+          .template AddSystem<drake::systems::sensors::ImageToLcmImageArrayT>();
+  image_to_lcm_image_array->set_name("converter");
+
+  const auto& cam_port =
+      image_to_lcm_image_array
+          ->DeclareImageInputPort<drake::systems::sensors::PixelType::kRgba8U>(
+              camera_name);
+  builder.Connect(camera->color_image_output_port(), cam_port);
+  auto image_array_lcm_publisher = builder.template AddSystem
+      (drake::systems::lcm::LcmPublisherSystem::Make<robotlocomotion
+      ::image_array_t>("DRAKE_RGBD_CAMERA_IMAGES", nullptr, 1.0/10 /*
+ * 10 fps publish period*/));
+  builder.Connect(image_to_lcm_image_array->image_array_t_msg_output_port(),
+                  image_array_lcm_publisher->get_input_port());
+
+  // Build the builder.
   std::unique_ptr<systems::Diagram<double>> diagram = builder.Build();
 
   // Create a context for this diagram and plant.
